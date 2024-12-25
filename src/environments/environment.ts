1a7abe79ebc5436f27a16810206b864cabdcc7cd
Oussema Hamouda <49439788+oussemaha@users.noreply.github.com>
2024-12-25 16:14:42 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5000/api/v1/',
+  apiUrl: 'http://ApplicationLB-1499880577.us-east-1.elb.amazonaws.com:5000/api/v1/',
 };
 
 /*
